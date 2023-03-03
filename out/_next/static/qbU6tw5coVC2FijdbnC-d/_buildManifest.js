0e433837fd99150121393abd7b9453e2600ed5c3
kercylan98 <kercylan@gmail.com>
2023-03-03 17:38:49 +0800

    添加out

---
+++ out/_next/static/qbU6tw5coVC2FijdbnC-d/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/299b4710ca2a9192.css","static/chunks/pages/index-7f89c64a5ceeda3c.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
