6a9cf427c2ce8c56b98352afaeb61ca4a5cf72c1
kercylan98 <kercylan@gmail.com>
2023-03-03 17:54:53 +0800

    test sync

---
+++ out/_next/static/VTYGOMIoziMi2qrcka13V/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/299b4710ca2a9192.css","static/chunks/pages/index-ad9bdab89292a329.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
